fe5a1a844bee9848287ff1ac79b55085aad5fbb4
Rogerio J. Gentil <rogerio_gentil@yahoo.com.br>
2018-02-06 00:47:52 -0200

    Converts to a CLI project
    - Adds Apache Commons CLI dependency; and
    - Adds Maven Dependency Plugin to copy dependencies to target folder.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # parser
+
 A Java Tool to analyse a Web Server Access Log file.
+
+
+## Construct
+
+### Requirements to build
+
+1. JDK 8
+2. Apache Maven 3
+
+### Building
+
+Just execute:
+
+    mvn clean install
+
+## Running
+
+Execute:
+
+    java -jar target/parser-2018.01.jar --help
--- src/main/java/com/ef/parser/Main.java
+++ src/main/java/com/ef/parser/Main.java
@@ -1,13 +1,45 @@
 package com.ef.parser;
 
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
 /**
  *
  * @author Rogerio J. Gentil
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Parser!");
-    }
+    /**
+     * Expected command-line:
+     * 
+     * <p>
+     * java -cp "parser.jar" com.ef.Parser --startDate=2017-01-01.13:00:00 --duration=hourly --threshold=100
+     * </p>
+     *
+     * @param args
+     * @throws org.apache.commons.cli.ParseException
+     */
+    public static void main(String[] args) throws ParseException {
+        Option help = Option.builder("h")
+                .longOpt("help")
+                .required(false)
+                .desc("HELP!")
+                .build();
 
+        Options options = new Options();
+        options.addOption(help);
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine commandLine = parser.parse(options, args);
+
+        if (commandLine.hasOption("help")) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("sb server", options);
+        }
+    }
 }
